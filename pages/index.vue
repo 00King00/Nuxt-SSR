cd4c48f7e250b20eb925635ed4ea9fed34b9ae6a
00King00 <mr.1bilous@gmail.com>
2019-04-26 13:56:33 +0300

    init progect

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div
+	h1 Test {{text}}
+	v-btn(@click="fetchText") Fetch data
+ 
+</template>
+<script>
+import {fireDb} from '~/plugins/firebase.js'
+export default {
+	data() {
+		return {
+			text: "default"
+		}
+	},
+	methods:{
+		async fetchText(){
+			const ref = fireDb.collection("Test").doc("test1");
+			let snap
+			try {
+				snap = await ref.get()
+				this.text = snap.data().text
+			} catch (e) {
+				console.error(e)
+			}
+		}
+	}
+}
+</script>
---
+++ plugins/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+if (!firebase.apps.length) {
+    const config = {
+		apiKey: "AIzaSyAcFXpJiet790i9nV__lEANexEwbnAgaIQ",
+		authDomain: "movie-list-ssr.firebaseapp.com",
+		databaseURL: "https://movie-list-ssr.firebaseio.com",
+		projectId: "movie-list-ssr",
+		storageBucket: "movie-list-ssr.appspot.com",
+		messagingSenderId: "276174071771"
+    }
+    firebase.initializeApp(config)
+    firebase.firestore()
+}
+const fireDb = firebase.firestore()
+export {fireDb}
